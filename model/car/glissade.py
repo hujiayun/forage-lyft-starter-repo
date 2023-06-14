d8625ccf553df7be1725fbc3cfc0a2cdf04cc0d1
hu jiayun <jihu9945@uni.sydney.edu.au>
2023-06-14 17:42:10 +1000

    refactor

--- model/car/glissade.py
+++ model/car/glissade.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from engine.willoughby_engine import WilloughbyEngine
+from model.engine.willoughby_engine import WilloughbyEngine
 
 
 class Glissade(WilloughbyEngine):
